e45339aa96a40f74537fcc4718df6a2f0a740345
Parth Sharma <parth261297@gmail.com>
2024-04-09 22:00:53 +0530

    feat: show bytes in the file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello World!")
+    let filename = env::args()
+        .nth_back(0)
+        .expect("Filename argument to be provided");
+
+    let metadata = fs::metadata(&filename).expect("Failed to read file metadata");
+
+    println!("{} {}", metadata.len(), filename);
 }
