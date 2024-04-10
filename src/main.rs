b489be943978233eb2c96ac35c327ad1ecdc5022
Parth Sharma <parth261297@gmail.com>
2024-04-10 19:59:06 +0530

    feat: add padding when printing results

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::collections::HashMap;
 use std::fs;
 
 #[derive(Parser)]
@@ -12,7 +13,10 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
     let total_files = args.filenames.len();
+
+    let mut byte_map: HashMap<String, u64> = HashMap::new();
     let mut total_bytes: u64 = 0;
     let mut print_count = args.print_count;
 
@@ -20,17 +24,25 @@ fn main() {
         print_count = true;
     }
 
-    for filename in args.filenames {
+    for filename in &args.filenames {
         if print_count {
             let metadata = fs::metadata(&filename).expect("Failed to read file metadata");
+            byte_map.insert(filename.to_string(), metadata.len());
             total_bytes += metadata.len();
-            println!("{} {}", metadata.len(), filename);
         }
     }
 
+    let total_bytes_width = total_bytes.to_string().len();
+
+    for filename in &args.filenames {
+        if print_count {
+            let bytes = byte_map.get(filename).unwrap();
+            println!("{:width$} {}", bytes, filename, width = total_bytes_width);
+        }
+    }
     if total_files > 1 {
         if print_count {
-            println!("{} total", total_bytes);
+            println!("{:width$} total", total_bytes, width = total_bytes_width);
         }
     }
 }
